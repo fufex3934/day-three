c533c0bf86c2e629f4bce5c3821730b463301fdc
fufex3934 <fufawakgari174@gmail.com>
2025-05-12 15:48:42 +0300

    login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,54 @@
-import React from 'react'
-
+'use client';
+import { useState } from "react";
 const Login = () => {
+  const [form,setForm] = useState({
+    email:'',
+    password:''
+  });
+
+  const [error,setError] = useState('');
+
+  const handleSubmit = async(e:React.FormEvent)=>{
+    e.preventDefault();
+    setError('');
+
+    const res = await fetch('/api/login',{
+      method:'POST',
+      headers:{'Content-Type':'application/json'},
+      body:JSON.stringify(form),
+    });
+
+    const data = await res.json();
+    if(!res.ok) return setError(data.error || 'signin failed');
+
+    window.location.href = '/dashboard';
+
+  }
   return (
-    <div>Login</div>
+     <form
+    className="p-4 max-w-sm  mx-auto space-y-4 bg-blue-100 mt-10 flex flex-col"
+    onSubmit={handleSubmit}
+    >
+      
+      
+      <input 
+      type="email" 
+      placeholder="Email"
+       className="p-2 border border-indigo-500 "
+       onChange={(e)=>setForm({...form,email:e.target.value})}
+      />
+      <input 
+      type="password" 
+      placeholder="Password"
+       className="p-2 border border-indigo-500 "
+       onChange={(e)=>setForm({...form,password:e.target.value})}
+      />
+      {error && <p className="text-red-500">{error}</p>} 
+      <button
+       type="submit"
+       className="bg-indigo-500 text-white py-1 hover:bg-indigo-400 cursor-pointer"
+       >SignIn</button>
+    </form>
   )
 }
 
